7b9fec1c159ef188599db5d69edb6af55c79ebb2
px <os11353325@gmail.com>
2017-05-01 23:24:19 +0800

    rails blog app, model controller router

---
+++ rails-blog/blog/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root 'articles#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles do
+    resources :comments
+  end
+end
