a607593e46e6d6996ba7f2752bb62d429f69f2d8
px <os11353325@gmail.com>
2017-05-11 22:38:58 +0800

    chapter-10

--- depot/app/controllers/carts_controller.rb
+++ depot/app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   # GET /carts
   # GET /carts.json
   def index
@@ -54,15 +54,20 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
-    respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @cart.destroy if @cart.id == session[:cart_id]
+      session[:cart_id] = nil
+      respond_to do |format|
+        format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+        format.json { head :no_content }
+      end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def invalid_cart
+      logger.error "attemp to access invalid cart #{params[:id]}"
+      redirect_to store_index_url, notice: 'invalid cart'
+    end
     def set_cart
       @cart = Cart.find(params[:id])
     end
--- depot/app/models/cart.rb
+++ depot/app/models/cart.rb
@@ -1,5 +1,18 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+  def total_price
+    line_items.to_a.sum{|i| i.total_price}
+  end
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product.id)
+    end
+    current_item
+  end
+
   private
   def set_cart
     @cart = Cart.find(session[:cart_id])
