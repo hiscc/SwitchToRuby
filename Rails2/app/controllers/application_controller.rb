f113549be6d9e890f25f32464710bcaa9f971c24
mrwh <mrwh2017@163.com>
2017-07-20 17:32:56 +0800

    cart

--- Rails2/app/controllers/application_controller.rb
+++ Rails2/app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :date_loaded
-  
+  helper_method :date_loaded, :counter
+  # before_action :counter
   def date_loaded
     return Time.now
   end
+
+  def counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
+    return session[:counter]
+  end
+
 end
--- Rails2/app/models/product.rb
+++ Rails2/app/models/product.rb
@@ -1,8 +1,18 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description,  :image_url, :price, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {with: %r{\.(gif|jpg|png)\Z}i, message: '必须是指定格式'
   }
 
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
